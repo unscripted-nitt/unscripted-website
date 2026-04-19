71e68a638186c1217845d9ba4568517dae579ef6
Akshay <aksahy.nitt04@gmail.com>
2026-04-19 10:28:06 +0530

    error correction

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'pages/login.html'),
         dashboard: resolve(__dirname, 'pages/dashboard.html'),
-        admin: resolve(__dirname, 'pages/admin.html'),
-        adminLogin: resolve(__dirname, 'pages/admin-login.html'),
         adminDashboard: resolve(__dirname, 'pages/admin-dashboard.html'),
         members: resolve(__dirname, 'pages/members.html'),
         videos: resolve(__dirname, 'pages/videos.html'),
